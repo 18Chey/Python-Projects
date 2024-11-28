6b88006fbf59cc45c3d39717034eea789a295cb1
Dinnerchrey <18chey@parmiters.herts.sch.uk>
2024-11-28 17:46:03 +0000

    Added insert and search functions.

--- trees.py
+++ trees.py
@@ -1,47 +1,56 @@
 class Tree:
-    def __init__(self, data):
+    def __init__(self, data: int) -> None:
         self.data = data
         self.left = None
         self.right = None
 
-    def add_left(self, data):
+    def add_left(self, data: int):
         self.left = Tree(data)
 
-    def add_right(self, data):
+    def add_right(self, data: int):
         self.right = Tree(data)
 
-    def show_tree(self):
-        left = None
-        right = None
-        if self.left != None:
-            left = self.left.data
-        if self.right != None:
-            right = self.right.data
 
-        print(f"{self.data} Left: {left} Right: {right}")
-        if self.left != None:
-            self.left.show_tree()
-        if self.right != None:
-            self.right.show_tree()
-
-
-def traverse(tree):
+def traverse(tree) -> None:
+    """Prints in-order traversal of tree"""
     if tree:
         traverse(tree.left)
-        print(tree.data)
+        print(tree.data, end=" ")
         traverse(tree.right)
 
 
-mytree = Tree(8)
-mytree.add_left(2)
-mytree.add_right(34)
-mytree.left.add_left(1)
-mytree.left.add_right(5)
-mytree.left.left.add_left(1)
-mytree.add_right(34)
-mytree.right.add_left(21)
-mytree.right.add_right(55)
-mytree.right.left.add_left(13)
-mytree.left.right.add_left(3)
+def insert(tree, data: int) -> Tree:
+    """Returns new version of tree with node inserted. If node data is equal to parent data, will be inserted on the left."""
+    if tree == None:
+        return Tree(data)
+    elif data > tree.data:
+        tree.right = insert(tree.right, data)
+        return tree
+    elif data <= tree.data:
+        tree.left = insert(tree.left, data)
+        return tree
 
-traverse(mytree)
+
+def search(tree, data) -> list[int | bool]:
+    """Returns path through tree and whether or not the item was found."""
+    if tree == None:
+        return [False]
+    elif tree.data == data:
+        return [True]
+    elif tree.data < data:
+        return [1] + search(tree.right, data)
+    elif tree.data > data:
+        return [0] + search(tree.left, data)
+
+
+def main() -> None:
+    mytree = None
+
+    while True:
+        mytree = insert(mytree, int(input("Data: ")))
+        traverse(mytree)
+        print(search(mytree, int(input("\nSearch"))))
+
+
+if __name__ == "__main__":
+    main()
